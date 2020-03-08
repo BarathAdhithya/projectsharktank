360005344da2cd82e8b175910addba9fb9b64231
BarathAdhithya <55236281+foolishbeing@users.noreply.github.com>
2020-03-08 11:33:21 +0530

    added a separate file for db connection

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'samplesharktank',
-    insecureAuth : true
-});
+let connection = require('./db/connection')
 
 let sql = `SELECT * FROM company WHERE industry_id=? and entrepreneur_gender_id=?`;
 connection.query(sql, [4,1], (error, results, fields) => {

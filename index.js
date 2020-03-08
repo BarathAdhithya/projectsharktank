7c59dbb0b18bc862f5772fd740fc9011b03e1f68
BarathAdhithya <55236281+foolishbeing@users.noreply.github.com>
2020-03-08 17:36:39 +0530

    added cors attribute to allow requests from localhost

---
+++ index.js
@@ -0,0 +1,19 @@
+//mongoosejs.com/docs/queries.html
+const express = require("express");
+require("./db/connection");
+const searchCompanyRouter = require("./router/search-company")
+var cors = require('cors')
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json()); //converts the response request.body into json
+app.use(searchCompanyRouter);
+
+app.listen(port, () => {
+  console.log("the port is up running at " + port);
+});
+

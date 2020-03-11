099b32b1e670d018689ca19dcf37e823399d0624
BarathAdhithya <55236281+foolishbeing@users.noreply.github.com>
2020-03-11 07:32:38 +0530

    added company details api

--- router/search-company.js
+++ router/search-company.js
@@ -4,58 +4,83 @@ const searchCompany = require("../model/search-company");
 const connection = require("../db/connection");
 
 router.get("/mainsearch/", async (req, res) => {
-  const seasonNo = req.query.seasonNo || '';
-  const episodeNo = req.query.episodeNo || '';
-  const deal = req.query.deal || '';
-  const gender = req.query.gender || '';
-  const investor = req.query.investor || '';
-  console.log("seasonNo :", seasonNo);
-  console.log("episodeNo :", episodeNo);
-  console.log("deal :", deal);
-  console.log("gender :", gender);
-  console.log("investor :", investor);
+  const seasonNo = req.query.seasonNo || "";
+  const episodeNo = req.query.episodeNo || "";
+  const deal = req.query.deal || "";
+  const gender = req.query.gender || "";
+  const investor = req.query.investor || "";
   let paramArray = [];
   if (true) {
-    let sql = 'SELECT * FROM company_stocks\
-    inner join company on company_stocks.company_id = company.id\
-    inner join entrepreneur_gender on company.entrepreneur_gender_id = entrepreneur_gender.id\
-    inner join episode_and_investors on company_stocks.company_id = episode_and_investors.company_id';
+    let mainSql =
+      "SELECT company_stocks.company_id,company_name,amount,episode_id,season_id FROM company_stocks\
+    inner join company on company_stocks.company_id = company.id";
+    let sql = "";
     try {
       if (seasonNo !== "") {
         sql += ` WHERE season_id=?`;
-        paramArray.push(seasonNo)
+        paramArray.push(seasonNo);
       }
-      if (episodeNo !== "" ) {
-        sql += ' and episode_id=?'
-        paramArray.push(episodeNo)
+      if (episodeNo !== "") {
+        sql += " and episode_id=?";
+        paramArray.push(episodeNo);
       }
       if (deal !== "") {
-        sql += ' and deal=?'
-        paramArray.push(deal)
+        sql += " and deal=?";
+        paramArray.push(deal);
       }
       if (gender !== "") {
-        sql += ' and entrepreneur_gender_id=?'
-        paramArray.push(gender)
+        mainSql +=
+          " inner join entrepreneur_gender on company.entrepreneur_gender_id = entrepreneur_gender.id";
+        sql += " and entrepreneur_gender_id=?";
+        paramArray.push(gender);
       }
       if (investor !== "") {
-        sql += ' and investor_id=?'
-        paramArray.push(investor)
+        mainSql +=
+          "  inner join episode_and_investors on company_stocks.company_id = episode_and_investors.company_id";
+        sql += " and investor_id=?";
+        paramArray.push(investor);
       }
-      console.log('sql :', sql);
+
+      mainSql += sql;
+      console.log("mainSql :", mainSql);
 
       let result = {};
-      connection.query(
-        sql,
-        paramArray,
-        (error, results, fields) => {
-          if (error) {
-            return console.error(error.message);
-          }
-          result = results[0];
-          console.log(result);
-          res.status(201).send(results);
+      connection.query(mainSql, paramArray, (error, results, fields) => {
+        if (error) {
+          return console.error(error.message);
         }
-      );
+        result = results[0];
+        console.log(result);
+        res.status(201).send(results);
+      });
+
+      // connection.end();
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  } else {
+    res.status(404).send("seasonNo, episodeNo, deal are required");
+  }
+});
+
+router.get("/companyDetails/", async (req, res) => {
+  const company_id = req.query.companyid
+  if (true) {
+    try {
+      let sql = `select company_id,company_name,amount,equity,valuation,sharks_count,amount_per_shark,deal,season_id,episode_id from company_stocks\
+      inner join company on company_stocks.company_id = company.id\ 
+      inner join entrepreneur_gender on company.entrepreneur_gender_id = entrepreneur_gender.id\
+      WHERE company_stocks.company_id=?`;
+      let result = {};
+      connection.query(sql,company_id, (error, results, fields) => {
+        if (error) {
+          return console.error(error.message);
+        }
+        // result = results[0];
+        // console.log(result);
+        console.log("results :", results);
+        res.status(201).send(results);
+      });
 
       // connection.end();
     } catch (error) {
